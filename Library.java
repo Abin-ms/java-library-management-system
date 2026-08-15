02c16f21702295b30ef0a6187466cea2972bd214
Abin-ms <abinn.mss@gmail.com>
2026-08-16 01:49:33 +0530

    aja

--- Library.java
+++ Library.java
@@ -4,14 +4,31 @@ public class Library {
     private ArrayList<Book> books;
     private ArrayList<Member> members;
 
-    public Library(){
+    public Library() {
         books = new ArrayList<>();
         members = new ArrayList<>();
     }
-   
-    public void addBook(Book book){
+
+    public void addBook(Book book) {
         books.add(book);
 
         System.out.println("Book added successfully");
+    }
+
+    public void viewBooks() {
+        for (Book book : books) {
+
+            System.out.println("Book id: " + book.getBookId());
+            System.out.println("Title of the book: " + book.getTitle());
+            System.out.println("Author's name: " + book.getAuthor());
+            System.out.println("Category: " + book.getCategory());
+
+            if (!book.isIssued()) {
+                System.out.println("Status: Available");
+            } else {
+                System.out.println("Status: Issued");
+            }
+
+        }
     }
 }
