1bb929c834b429e10e4bc1936c93daf22d050655
Abin-ms <abinn.mss@gmail.com>
2026-08-16 01:07:20 +0530

    initial commit

---
+++ Book.java
@@ -0,0 +1,57 @@
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private String category;
+    private boolean isIssued;
+
+    Book(int bookId, String title, String author, String category) {
+
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void setIssued(boolean isIssued) {
+        this.isIssued = isIssued;
+    }
+
+}
