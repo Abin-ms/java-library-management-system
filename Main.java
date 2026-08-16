2e7d4c21eb713da6ce5795b6e493b9ce7bd7c8e7
Abin-ms <abinn.mss@gmail.com>
2026-08-16 22:25:52 +0530

    designing a console user interface

--- Main.java
+++ Main.java
@@ -1,19 +1,60 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Library library = new Library();
+        // Book book1 = new Book(101, "Clean Code", "Robert Martin", "Programming");
+        // Book book2 = new Book(102, "Effective Java", "Joshua Bloch", "Programming");
+        // Book book3 = new Book(103, "The Alchemist", "Paulo Coelho", "Fiction");
+
+        // library.addBook(book1);
+        // library.addBook(book2);
+        // library.addBook(book3);
+
+        // library.viewBooks();
+
+        // library.searchBook(103);
+
+        // library.searchBook(99);
+
+        // Library library = new Library();
+
+        // Book book1 = new Book(101, "Clean Code", "Robert Martin", "Programming");
+
+        // Member member1 = new Member(1, "Abin", "abin@gmail.com", "9876543210");
+
+        // library.addBook(book1);
+        // library.addMember(member1);
+
+        // library.issueBook(101, 1);
+
+        // library.viewBooks();
+
+        // library.returnBook(101);
+
+        // library.viewBooks();
+
+         Scanner sc = new Scanner(System.in);
         Library library = new Library();
-        Book book1 = new Book(101, "Clean Code", "Robert Martin", "Programming");
-        Book book2 = new Book(102, "Effective Java", "Joshua Bloch", "Programming");
-        Book book3 = new Book(103, "The Alchemist", "Paulo Coelho", "Fiction");
 
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
+        int choice;
 
-        library.viewBooks();
+do {
+    System.out.println("\n===== LIBRARY MANAGEMENT SYSTEM =====");
+    System.out.println("1. Add Book");
+    System.out.println("2. Add Member");
+    System.out.println("3. View Books");
+    System.out.println("4. View Members");
+    System.out.println("5. Search Book");
+    System.out.println("6. Search Member");
+    System.out.println("7. Issue Book");
+    System.out.println("8. Return Book");
+    System.out.println("9. Exit");
 
-        library.searchBook(103);
+    System.out.print("Enter your choice: ");
+    choice = sc.nextInt();
 
-        library.searchBook(99);
+} while (choice != 9);
     }
 }
